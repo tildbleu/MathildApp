101e64ead11f558971281031f5dbfa11fce77469
math-bl <31114034+math-bl@users.noreply.github.com>
2018-02-22 00:09:15 +0100

    Lifeback tutorial 1 First part, add log message. Add lod tag definition

--- app/src/main/java/com/example/mathildapp/MainActivity.java
+++ app/src/main/java/com/example/mathildapp/MainActivity.java
@@ -3,14 +3,30 @@ package com.example.mathildapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
     }
 
     public void launchCount(View view) {
